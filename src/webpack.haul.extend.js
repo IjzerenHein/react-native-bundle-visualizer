8ae4c2e3e5402a6a623eedc4d8da211bc390769c
IjzerenHein <hrutjes@gmail.com>
2017-09-03 22:10:35 +0200

    Fixed error when running on node < 8.4, due to use of spread operator

--- src/webpack.haul.extend.js
+++ src/webpack.haul.extend.js
@@ -3,11 +3,11 @@ const originalConfig = require('../../../webpack.haul.js');
 
 module.exports = function(context, config) {
 	config = originalConfig(context, config);
-	return {
-		...config,
-		plugins: [
-			...(config.plugins || []),
-			new Visualizer()
-		]
-	};
+	return Object.assign(
+		{},
+		config,
+		{
+			plugins: (config.plugins || []).concat([new Visualizer()])
+		}
+	);
 };
--- src/webpack.haul.js
+++ src/webpack.haul.js
@@ -3,22 +3,21 @@ const Visualizer = require('webpack-visualizer-plugin');
 module.exports = ({platform}, {module, plugins}) => {
 	return {
 		entry: `./index.${platform}.js`,
-		module: {
-			...module,
-			rules: [
-				{
-					test: /\.js?$/,
-					exclude: '/node_modules/',
-					use: [{
-						loader: 'babel-loader'
-					}]
-				},
-				...module.rules
-			]
-		},
-		plugins: [
-			...(plugins || []),
-			new Visualizer()
-		]
+		module: Object.assign(
+			{},
+			module,
+			{
+				rules: [
+					{
+						test: /\.js?$/,
+						exclude: '/node_modules/',
+						use: [{
+							loader: 'babel-loader'
+						}]
+					}
+				].concat(module.rules || [])
+			}
+		),
+		plugins: (plugins || []).concat([new Visualizer()])
 	};
 };
