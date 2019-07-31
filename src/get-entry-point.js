4fa046d9e1f354cc8b39db4920c0fe97c51c41d4
Hein Rutjes <hrutjes@gmail.com>
2019-07-31 13:05:34 +0200

    Fixed entry point not found for expo projects

--- src/get-entry-point.js
+++ src/get-entry-point.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 
 function readPackageJSON() {
-	return JSON.parse(fs.readFileSync('./package.json'));
+  return JSON.parse(fs.readFileSync('./package.json'));
 }
 
 function getEntryPoint() {
-	const pkgJSON = readPackageJSON();
-	return pkgJSON.main || './index.js';
+  const pkgJSON = readPackageJSON();
+  let entry = pkgJSON.main || 'index.js';
+  if (entry[0] !== '.' && entry[0] !== '/' && entry[0] !== '\\') {
+    entry = './' + entry;
+  }
+  return entry;
 }
 
 module.exports = {
-	getEntryPoint,
+  getEntryPoint
 };
